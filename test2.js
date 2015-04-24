6d530bf3e6b9d69b4768deef46af1e2f0a84357d
img src=x <tunz7unz@naver.com>
2015-04-24 09:42:14 +0900

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,1 +1,33 @@
-[{"name": "Fred", "age": 10, "height": 120},{"name": "Ahmed", "age": 18, "height": 178}]
+{ "type": "FeatureCollection",
+    "features": [
+      { "type": "Feature",
+        "geometry": {"type": "AAAA", "coordinates": [102.0, 'aa']},
+        "properties": {"prop0": "<a click='asdf\"aaa\"asdfasdfaasdf!@#'>aaaaaaa</a>"}
+        },
+      { "type": "Feature",
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [102.0, -1], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
+            ]
+          },
+        "properties": {
+          "prop0": "value0",
+          "prop1": 0.0
+          }
+        },
+      { "type": "Feature",
+         "geometry": {
+           "type": "Polygon",
+           "coordinates": [
+             [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0],
+               [100.0, 1.0], [100.0, 0.0] ]
+             ]
+         },
+         "properties": {
+           "prop0": "value0",
+           "prop1": {"this": "that"}
+           }
+         }
+       ]
+     }
