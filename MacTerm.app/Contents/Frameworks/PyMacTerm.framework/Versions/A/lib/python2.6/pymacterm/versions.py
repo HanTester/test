4a6a26a6e67ae56db43c0121a27ebcbd39259563
Choongwoo Han <cwhan.tunz@gmail.com>
2015-04-29 08:16:48 +0900

    a

--- MacTerm.app/Contents/Frameworks/PyMacTerm.framework/Versions/A/lib/python2.6/pymacterm/versions.py
+++
@@ -1,7 +0,0 @@
-# automatically-generated Python file with basic version data
-major_version = 4
-minor_version = 1
-subminor_version = 0
-alpha_beta = 'b'
-build_number = 20150308
-prefs_version = 7
